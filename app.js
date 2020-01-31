a1ebd5307fbef9c061a5bf6ad68083d0a01e82b4
Rochowski <rochowskim@media-saturn.com>
2020-01-31 16:24:20 +0100

    configuration for MONGODB_URI added

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const passport = require("passport");
 require("./configs/passport");
 
 mongoose
-  .connect('mongodb://localhost/my-car-app-react', {useNewUrlParser: true})
+  .connect(process.env.MONGODB_URI || 'mongodb://localhost/my-car-app-react', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -47,7 +47,7 @@ app.use(require('node-sass-middleware')({
       
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
