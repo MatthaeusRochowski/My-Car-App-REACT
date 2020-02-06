4b83c89ca83fffbf0553d7cf4f5719d94719427b
Malte Schenkling <schenkling@mediamarktsaturn.com>
2020-02-06 08:59:47 +0100

    restructured fuelstation code

--- client/src/components/FuelStations.js
+++ client/src/components/FuelStations.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import FuelStationMap from './FuelStationMap';
 
 export default class FuelStations extends Component {
-  //test mode is controlled by servers routes/fuelApi.js
+  //test mode is controlled by servers routes/fuelApi.js with the intend to limit the api calls to tankerkönig 
   state = {
     zip: "",
     fuelstations: [],
@@ -18,8 +18,8 @@ export default class FuelStations extends Component {
 
   handleFormSubmit = (event) => {
     event.preventDefault();
-    let fuelStationRequestUrl = "/api/getFuelStations?zip=";
-    fuelStationRequestUrl += (this.state.zip === "") ? "85053" : this.state.zip;
+    let fuelStationRequestUrl = "/api/getFuelStations?zip=" + (this.state.zip === "" ? "85053" : this.state.zip);
+    //console.log(fuelStationRequestUrl);
     console.log("Frontend: FuelStations: FuelStations.js  - before Backend call");
     axios.get(fuelStationRequestUrl)
     .then(response => {
@@ -34,29 +34,35 @@ export default class FuelStations extends Component {
     })
   }
 
-  render() {
-    console.log("Frontend: FuelStations: FuelStations.js - render invoked");
-    console.log("Frontend: FuelStations: FuelStations.js - testMode: ", this.state.testMode);
-    console.log("Frontend: FuelStations: FuelStations.js - fuelstation state:", this.state.fuelstations);
-    console.log("Frontend: FuelStations: FuelStations.js - sortOrder state:", this.state.sortOrder);
-    let currFuelStations = this.state.fuelstations
-    currFuelStations.map((oneFuelStation, index) => {
-      if (index === 0) {
-        return oneFuelStation.color = 'red';
-      }
-      else {
-        if (index%2 === 1) {
-          return oneFuelStation.color = 'lightgreen';
-        }
-        else {
-          return oneFuelStation.color = 'lightblue';
-        }
-      }
-    });
+  calculateMapCenter = () => {
+    let googleMapsCenter = {lat: 48.731, lng: 11.187};
+    let currFuelStations = this.state.fuelstations;
 
+    if (currFuelStations.length > 0) {
+      let googleMapsCenter = currFuelStations.reduce(function (acc, cur) {
+        let res = {minLat: cur.lat, maxLat: cur.lat, minLng: cur.lng, maxLng: cur.lng}
+        acc.minLat > cur.lat ? res.minLat = cur.lat : res.minLat = acc.minLat
+        acc.minLng > cur.lng ? res.minLng = cur.lng : res.minLng = acc.minLng
+        acc.maxLat < cur.lat ? res.maxLat = cur.lat : res.maxLat = acc.maxLat
+        acc.maxLng < cur.lng ? res.maxLng = cur.lng : res.maxLng = acc.maxLng
+        return res;
+      },{minLat: 999999, maxLat: 0, minLng: 999999, maxLng: 0});
+
+      let centerLat = Math.round((googleMapsCenter.minLat+((googleMapsCenter.maxLat-googleMapsCenter.minLat)/2))*1000)/1000;
+      let centerLng = Math.round((googleMapsCenter.minLng+((googleMapsCenter.maxLng-googleMapsCenter.minLng)/2))*1000)/1000;
+      googleMapsCenter = { lat: centerLat, lng: centerLng };
+    }
+    //console.log("google maps center coords: ", googleMapsCenter);
+    return googleMapsCenter;
+  }
+
+  prepareFuelStations = () => {
+    //console.log("Frontend: FuelStations: FuelStations.js - testMode:", this.state.testMode ? 'ON' : 'OFF');
+    //console.log("Frontend: FuelStations: FuelStations.js - fuelstation state:", this.state.fuelstations);
+    //console.log("Frontend: FuelStations: FuelStations.js - sortOrder state:", this.state.sortOrder);
     let googleMapsMarker = {lat: 48.731, lng: 11.187};
-    let googleMapsMarkerCenterInterim = {};
-    let googleMapsMarkerCenter = {lat: 48.731, lng: 11.187};
+    let currFuelStations = this.state.fuelstations;
+
     if (currFuelStations.length > 0) {
       let sortOrder = this.state.sortOrder;
       googleMapsMarker = currFuelStations.sort(function(stationA, stationB) {
@@ -95,64 +101,47 @@ export default class FuelStations extends Component {
             return 0;
         }
       }).map((oneFuelStation, index) => {
-        if (index === 0) {
-          if (oneFuelStation.hasOwnProperty('place')) {
-            oneFuelStation['city'] = oneFuelStation.place;  //renamed place to city as this solves an unexpected error in index.js
-            delete oneFuelStation['place'];
-          }
-          return {index: index, ...oneFuelStation, color: 'red'}
+        oneFuelStation['index'] = index;
+        if (oneFuelStation.hasOwnProperty('place')) {
+          oneFuelStation['city'] = oneFuelStation.place;  //renamed place to city as this solves an unexpected error in index.js
+          delete oneFuelStation['place'];
         }
-        else {
-          if (oneFuelStation.hasOwnProperty('place')) {
-            oneFuelStation['city'] = oneFuelStation.place;  //renamed place to city as this solves an unexpected error in index.js
-            delete oneFuelStation['place'];
-          }
-          return {index: index, ...oneFuelStation, color: 'blue'}
-        }
+        (index === 0) ? oneFuelStation['color'] = "red" : oneFuelStation['color'] = "blue";
+        return oneFuelStation;
       });
-      console.log("sorted markers: ", googleMapsMarker);
-
-      googleMapsMarkerCenterInterim = googleMapsMarker.reduce(function (acc, cur) {
-        let res = {minLat: cur.lat, maxLat: cur.lat, minLng: cur.lng, maxLng: cur.lng}
-        acc.minLat > cur.lat ? res.minLat = cur.lat : res.minLat = acc.minLat
-        acc.minLng > cur.lng ? res.minLng = cur.lng : res.minLng = acc.minLng
-        acc.maxLat < cur.lat ? res.maxLat = cur.lat : res.maxLat = acc.maxLat
-        acc.maxLng < cur.lng ? res.maxLng = cur.lng : res.maxLng = acc.maxLng
-        return res;
-      },{minLat: 999999, maxLat: 0, minLng: 999999, maxLng: 0});
-
-      let centerLat = Math.round((googleMapsMarkerCenterInterim.minLat+((googleMapsMarkerCenterInterim.maxLat-googleMapsMarkerCenterInterim.minLat)/2))*1000)/1000;
-      let centerLng = Math.round((googleMapsMarkerCenterInterim.minLng+((googleMapsMarkerCenterInterim.maxLng-googleMapsMarkerCenterInterim.minLng)/2))*1000)/1000;
-      googleMapsMarkerCenter = { lat: centerLat, lng: centerLng };
-      //console.log("google maps center coords: ", googleMapsMarkerCenter);
+      //console.log("sorted markers: ", googleMapsMarker);
     }
+    return googleMapsMarker;
+  }
+
+  render() {
+    console.log("Frontend: FuelStations: FuelStations.js - render invoked");
+    let currFuelStations = this.state.fuelstations;
+    let googleMapsCenter = this.calculateMapCenter();
+    let googleMapsMarker = this.prepareFuelStations();
 
     let mapsForm = (
-      <form onSubmit={this.handleFormSubmit}>
-        <label htmlFor="zip">Postleitzahl: </label>
-        <input name="zip" type="text" minLength="5" maxLength="5" onChange={this.handleInputChanges} placeholder="z.B. 85083" />
-        <label htmlFor="sortOrder">Sortierung nach: </label>
+      <div>
+        <form onSubmit={this.handleFormSubmit}>
+          <label htmlFor="zip">Postleitzahl: </label>
+          <input name="zip" type="text" minLength="5" maxLength="5" onChange={this.handleInputChanges} placeholder="z.B. 85083" />
+          <label htmlFor="sortOrder">Sortierung nach: </label>
+          <button type="submit">Request</button>
+        </form>
         <select name="sortOrder" onChange={this.handleInputChanges}>
           <option value="distance">Entfernung</option>
           <option value="e5price">E5-Preis</option>
           <option value="e10price">E10-Preis</option>
           <option value="dieselprice">Diesel-Preis</option>
         </select>
-        <button type="submit">Request</button>
-      </form>
-      ) 
+      </div>
+    ) 
 
     return (
       <div>
         <h1>Tankstellen im Umkreis</h1>
         {mapsForm}
-        <div id="googleMaps">
-          {currFuelStations.length > 0 ? (
-            <FuelStationMap center={googleMapsMarkerCenter} markers={googleMapsMarker} />
-            ) : (
-            console.log("Frontend: FuelStations: FuelStations.js - empty fuelstations state (array) - maps")
-          )}
-        </div>
+        {currFuelStations.length > 0 ? <FuelStationMap center={googleMapsCenter} markers={googleMapsMarker} /> : console.log("Frontend: FuelStations: FuelStations.js - empty fuelstations state (array) - maps")}
       </div>
     )
   }
